3b8152784b16f38cb965b26c90b57dae793e17e8
Gilmez77 <64424884+Gilmez77@users.noreply.github.com>
2020-04-28 20:32:38 -0700

    Agregar archivos de proyecto.

---
+++ Nombres/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Nombres
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Nombres = new string[10];
+            for (int i = 0; i < Nombres.Length; i++)
+            {
+                Console.Write($"Escriba el Nombre {(i + 1)}:");
+                Nombres[i] = Console.ReadLine();
+            }
+            Console.WriteLine("la lista de nombres es");
+            for (int i = 0; i < Nombres.Length; i++)
+            {
+                Console.WriteLine(Nombres[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
